ad7652ada5c8926ebc885de9cdc6374df294b13f
Alder Furtado <aldermarcel12@gmail.com>
2024-11-25 19:19:37 -0300

    feat: adding napier lib

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -29,6 +29,7 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.napier.aakira)
             implementation(libs.kotlinx.coroutines.core)
         }
         commonTest.dependencies {
