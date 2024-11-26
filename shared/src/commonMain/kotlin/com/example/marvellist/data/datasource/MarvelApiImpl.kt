5476cd84c4bcea202dcb69b210885ec5189ff765
Alder Furtado <aldermarcel12@gmail.com>
2024-11-26 15:55:03 -0300

    feat: set default url

--- shared/src/commonMain/kotlin/com/example/marvellist/data/datasource/MarvelApiImpl.kt
+++ shared/src/commonMain/kotlin/com/example/marvellist/data/datasource/MarvelApiImpl.kt
@@ -8,7 +8,7 @@ import io.ktor.client.statement.HttpResponse
 
 class MarvelApiImpl(private val http: Http): MarvelApi {
     override suspend fun getCharacters(): MarvelResponse {
-        val data = http.get<HttpResponse, () -> Unit>("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=ec42ff12023878ab39a533cb3fcb5f44&hash=c2ca20873369bbbcdf9103d060ad390d&limit=100", {})
+        val data = http.get<HttpResponse, () -> Unit>("", {})
         val body: MarvelResponse = data.body()
         NapierAdapter.logDebug(body.toString())
         return body
