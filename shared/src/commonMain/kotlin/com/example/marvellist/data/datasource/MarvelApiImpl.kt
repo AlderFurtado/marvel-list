4904bb2379678258493cfcc30cb0d157907c01e8
Alder Furtado <aldermarcel12@gmail.com>
2024-12-11 11:05:06 -0300

    feat: changing strategy MD5 Generate

--- shared/src/commonMain/kotlin/com/example/marvellist/data/datasource/MarvelApiImpl.kt
+++ shared/src/commonMain/kotlin/com/example/marvellist/data/datasource/MarvelApiImpl.kt
@@ -2,9 +2,8 @@ package com.example.marvellist.data.datasource
 
 import com.example.marvellist.BuildKonfig
 import com.example.marvellist.data.model.MarvelResponse
-import com.example.marvellist.infra.http.Http
-import com.example.marvellist.infra.logging.NapierAdapter
-import com.example.marvellist.util.MD5Generate
+import com.example.marvellist.infra.Http.Http
+import com.example.marvellist.infra.Logging.NapierAdapter
 import io.ktor.client.call.body
 import io.ktor.client.statement.HttpResponse
 
@@ -17,18 +16,22 @@ class MarvelApiImpl(private val http: Http) : MarvelApi {
     }
 
     override suspend fun getCharacters(offset: Int, limit: Int): MarvelResponse {
-        val data = http.get<HttpResponse, () -> Unit>(stringUrlBuilder("v1/public/characters", offset, limit), {})
+        NapierAdapter.logDebug("getCharacters(offset=$offset, limit=$limit)")
+
+        val url = stringUrlBuilder("v1/public/characters", offset, limit)
+
+        NapierAdapter.logDebug("url = $url")
+        val data = http.get<HttpResponse, () -> Unit>(url){}
         val body: MarvelResponse = data.body()
         NapierAdapter.logDebug(body.toString())
-        return body
+        return body.also {
+            NapierAdapter.logDebug("")
+        }
     }
 
     private fun stringUrlBuilder(url: String, offset: Int, limit: Int):String {
-        val hash = generateMD5Hash()
+        val hash = BuildKonfig.MD5_KEY
         return "$url?ts=1&apikey=$publicKey&hash=$hash&limit=$limit&offset=$offset"
     }
-    private fun generateMD5Hash():String {
-        val input = "$ts$privateKey$publicKey"
-        return MD5Generate.invoke(input)
-    }
+
 }
