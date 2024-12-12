7825def4093414ec2df6ae9fc221df47e71db178
Alder Furtado <aldermarcel12@gmail.com>
2024-12-12 14:53:25 -0300

    Add CharacterListView

--- iosApp/iosApp/View/CharacterList/CharacterListView.swift
+++ iosApp/iosApp/View/CharacterList/CharacterListView.swift
@@ -18,8 +18,11 @@ struct CharacterListView: View {
             Task {
                 do {
                     let data = try await Factories().getCharacterBasicInfoListUseCase().invoke(offset: 0, limit: 100)
-                        NSLog("App started with state: %@", "\(data)")
-                        self.characterBasicInfoList = data
+                    let dataT = data.map{ character in
+                        CharacterBasicInfo(name: character.name, imageUrl: replaceHttpToHttps(input: character.imageUrl), description: character.description_, lastModified: character.lastModified, numberOfEvents: character.numberOfEvents, numberOfComics: character.numberOfComics, numberOfSeries: character.numberOfSeries, numberOfStories: character.numberOfStories)
+                    }
+                    NSLog("DataT", dataT)
+                    self.characterBasicInfoList = dataT
                 } catch {
                     print("Failed to fetch data: \(error.localizedDescription)")
                 }
@@ -28,9 +31,57 @@ struct CharacterListView: View {
 
     }
     
+    func replaceHttpToHttps(input: String) -> String{
+        return input.replacingOccurrences(of: "http", with: "https")
+    }
+    
     var body: some View {
         List(characterBasicInfoList, id: \.self) { item in
-            Text(item.name)
+            let imageUrl = item.imageUrl+"/portrait_xlarge.jpg"
+            HStack {
+                AsyncImage(url: URL(string: imageUrl)) { phase in
+                            if let image = phase.image {
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .cornerRadius(4)
+                                    .frame(width: 130, height: 130)
+                            } else if phase.error != nil {
+                                Text("Failed to load image")
+                            } else {
+                                ProgressView() // Placeholder while loading
+                            }
+                        }
+                        .frame(width: 140, height: 140)
+             
+                VStack(alignment:.leading,spacing: 4, content: {
+                    Text(item.lastModified)
+                        .font(.footnote).foregroundStyle(.gray)
+                    Text(item.name)
+                        .font(.title3)
+                        .bold()
+                    Text(item.description_ ?? "N/A")
+                        .font(.caption)
+                        .lineLimit(2)
+                    Spacer()
+                    HStack{
+                        Spacer()
+                        Button(action: {
+                            print("\(item) button clicked")
+                        }){
+                            Text("More")
+                                .font(.footnote)
+                                .padding(.horizontal,20)
+                                .padding(.vertical,8)
+                                .background(.red)
+                                .foregroundColor(.white)
+                                .cornerRadius(4)
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                })
+            
+            }
+         
         }.task {
             getCharacterList()
         }
